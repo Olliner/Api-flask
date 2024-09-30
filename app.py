53eea8e727d8c23d7e27624019f7a2ccd82f86dc
Daniel Olliner <danielrosabarcellos17.com>
2024-09-29 21:25:07 -0300

    Testando sem cors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import re  # Para validação de e-mail
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
